d7c20a27496933f9c1e690e5d51959c45231458a
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2024-06-11 21:34:05 +0300

    Updated CSS, JavaScript, and HTML files, and modified PHP script.

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,7 +1,7 @@
 /**
 * PHP Email Form Validation - v3.7
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
+* URL: https://surgeafrica.co.ke/php-email-form/
+* Author: Surgeafrica.co.ke
 */
 (function () {
   "use strict";
